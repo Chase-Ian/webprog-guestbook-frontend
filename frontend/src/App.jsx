3cf06635d731519d6a2ae9d91dd0552bb1babc77
Chase Ian Famisaran <clfamisaran@student.apc.edu.ph>
2026-02-12 18:46:47 +0000

    revert

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const API = "https://webprog-guestbook-backend.vercel.app";
+const API = "https://webprog-guestbook-backend.vercel.app/guestbook";
 
 export default function App() {
   const [entries, setEntries] = useState([]);
